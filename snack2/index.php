22378aa5e22b690e135d96c2efbd4185fc371364
shrekt444 <andrea.maie444@gmail.com>
2021-05-18 17:21:52 +0200

    continuo es 2

--- snack2/index.php
+++ snack2/index.php
@@ -1,12 +1,27 @@
 <?php 
     $name = $_GET['nome'];
-    echo $name;
 
     $mail = $_GET['mail'];
-    echo $mail;
 
-    $age = $_GET['eta'];
-    echo $age;
-    
-
+    $age = $_GET['eta'];   
 ?>
+
+<?php echo $name ?> - <?php echo $mail ?> - <?php echo $age ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php if (strlen($name) > 3) && () && { ?>
+        Accesso consentito;
+        else
+    <?php } ?> 
+    
+</body>
+</html>
